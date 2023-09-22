2cabc3676d58fabba23ce64f749282f810d89567
SoumyajeetMazumdar <soumyajeetmazumdar@gmail.com>
2023-09-22 21:11:51 +0530

    Home, About done, routing using Layout File

---
+++ 04-react-router/src/App.jsx
@@ -0,0 +1,9 @@
+// import Header from "./components/Header/Header";
+// import Footer from "./components/Footer/Footer";
+// import Home from "./components/Home/Home";
+
+function App() {
+  return <></>;
+}
+
+export default App;
